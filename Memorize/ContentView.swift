3611ff72e40fede7ddb4e263355d2e5805c1f788
Chris Wong <chriswong.broadband@gmail.com>
2024-05-08 06:59:38 +0800

    Array filled with ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻", "🎃", "🕷️", "😈", "😈"  ]
     var body: some View {
         HStack {
-            CardView(isFaceUp: false)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -26,14 +26,15 @@ struct ContentView: View {
 
 
 struct CardView: View {
-    @State var isFaceUp = false
+    let content: String
+    @State var isFaceUp = true
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2.0)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 // the background color populated by outer view
                 base.fill()
